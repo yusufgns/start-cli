32a58b81be5760b5553375306b78e7a8e769c2ff
Yusuf Güneş <gunesyusuf838@gmail.com>
2024-01-13 16:41:59 +0300

    fix: package and tsconfig

--- packages/index.js
+++ packages/index.js
@@ -1,18 +1,13 @@
 import * as p from '@clack/prompts';
 import { setTimeout } from 'node:timers/promises';
 import color from 'picocolors';
-
 function onCancel() {
-	p.cancel('Operation cancelled.');
-	process.exit(0);
+    p.cancel('Operation cancelled.');
+    process.exit(0);
 }
-
 async function main() {
     console.clear();
     await setTimeout(1000);
-
     p.intro(`${color.bgCyan(color.black(' changesets '))}`);
-    
 }
-
 main().catch(console.error);
